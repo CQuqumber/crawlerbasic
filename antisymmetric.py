23e378d63bf0459bc82e0ce61df68a22cc294c76
Kueipo <cququmber@gmail.com>
2017-01-02 18:55:51 +0800

    anti symmetric matrix

---
+++ antisymmetric.py
@@ -0,0 +1,43 @@
+# By Dimitris_GR from forums
+# Modify Problem Set 31's (Optional) Symmetric Square to return True 
+# if the given square is antisymmetric and False otherwise. 
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(A):
+    if len(A) == 0:
+        return True
+    if len(A) != len(A[0]):
+        return False
+    x = 0
+    for i in A:
+        y = 0
+        for j in A:
+            if A[x][y] != -1* A[y][x]:
+                return False
+            y = y + 1
+        x = x + 1
+    return True
+
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
